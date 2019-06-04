cfc7c1387c313038fcd5bee097f118a1911be2b0
Fabricio <fabricio.1337@gmail.com>
2019-06-04 16:12:55 -0300

    Update Player.cs
    Para realizar movimento uniforme e independente de framerate é necessário multiplicar os valores pelo delta fornecido pela engine.

--- Player.cs
+++ Player.cs
@@ -30,7 +30,15 @@ public class Player : KinematicBody2D
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
-        this.Position += GRAVITY;
+        // do jeito que estava quanto mais frames sua máquina for capaz de processar
+        // mais rapido o Player irá se mexer, e isso não é ideal
+        // é necessário multiplicar toda unidade de movimento pelo 'delta' fornecido pela engine
+        // assim vc transforma o GRAVITY e VELOCITY que devem estar na unidade distancia/segundo
+        // para a unidade distancia/frame
+        // OBS.: se esses valores foram testados por feeling para descobrir qual fica mais legal em jogo
+        //  será necessário realizar os teste de novo, desta vez mantendo em mente que a unidade é distancia/segundo
+        
+        this.Position += GRAVITY * delta;
           // Called every frame. Delta is time since last frame.
         // Update game logic here.
         velocity = new Vector2();
@@ -44,22 +52,24 @@ public class Player : KinematicBody2D
             (GetNode( "chicken" ) as Sprite).SetFlipH(false);
         }
         
+        // para rotação também é importante multiplicar por delta
+        // para transformar a unidade de rotação de graus(?)/segundo para graus/frame
         if(Input.IsActionPressed("ui_down") || Input.IsKeyPressed((int)KeyList.S)) {
-            targetRotation -= 0.05;
+            targetRotation -= 0.05 * delta;
         }
         
 
         if(Input.IsActionPressed("ui_up") || Input.IsKeyPressed((int)KeyList.W)) {
-            targetRotation += 0.05;
+            targetRotation += 0.05 * delta;
         }
         
-        this.Position += velocity;
+        this.Position += velocity * delta; // ao usar o MoveAndCollide isso aqui se tornaria redundante?
 
         target.SetRotation((float) targetRotation);
         target.SetPosition(new Vector2(targetRadius*(float) Math.Cos(targetRotation), targetRadius*(float) Math.Sin(targetRotation)));
 
     
         //this.SetPosition(this.Position); 
-        MoveAndCollide(velocity);
+        MoveAndCollide(velocity * delta);
   }
 }
